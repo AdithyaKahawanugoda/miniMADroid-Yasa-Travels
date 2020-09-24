35ef5d42b5aaa4fb33d3eb253d28818e48abe9ca
yasa16 <yasasminikeshala@gmail.com>
2020-09-24 23:14:55 +0530

    Yasasmi - Initial Commit

---
+++ app/src/main/java/com/example/yasatravels/EditGuideActivity.java
@@ -0,0 +1,14 @@
+package com.example.yasatravels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_guide);
+    }
+}
---
+++ app/src/main/java/com/example/yasatravels/GuideDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.example.yasatravels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class GuideDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_details);
+    }
+
+    public void contactHandler(View view){}
+}
