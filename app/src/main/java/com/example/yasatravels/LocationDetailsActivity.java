12a9a0e8eb0d8bf312805ef7cbe644164ed62c06
LihiniPriyasarani <lihinipriyasarani123@gmail.com>
2020-09-24 10:35:44 -0700

    Lihini initial commit

---
+++ app/src/main/java/com/example/yasatravels/LocationDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.example.yasatravels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LocationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_details);
+    }
+
+
+}
